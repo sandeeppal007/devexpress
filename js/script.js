955d6278dfc4304998acf8a5bd2d8bc2d9f4c01a
sandeep pal <sandeeppal7208@gmail.com>
2022-12-10 09:46:49 +0530

    logo

--- js/script.js
+++ js/script.js
@@ -2,19 +2,21 @@
 
 const navToggle = document.querySelector(".hemburger");
 
+
 const menu = document.querySelector(".menu");
 const topNav = document.querySelector(".top-nav");
 const cross = document.querySelector(".cross");
 // const links = document.querySelector(".main-nav");
 
 var x = document.getElementById("myTopnav");
+var topnavlogo = document.getElementById("topnavlogo");
 var heroBg = document.getElementById("hero-bg");
   
    function menuFunc(){
 x.classList.toggle("hero-nav-item")
 topNav.style.position = "static";
 menu.style.display = "none";
-// heroBg.classList.toggle("remove-bg-text")
+
    }
 
    function crossFunc(){
@@ -22,7 +24,6 @@ menu.style.display = "none";
     menu.style.display = "block";
     topNav.style.position = "fixed";
    
-    // x.classList.remove("remove-bg-text")
    }
 
    function navbarRealease(){
@@ -31,3 +32,8 @@ menu.style.display = "none";
 navToggle.addEventListener('click', menuFunc)
 cross.addEventListener('click', crossFunc)
  
+window.scroll(function(){
+   if ($(this).scrollTop() > 50) {
+
+   } 
+});
